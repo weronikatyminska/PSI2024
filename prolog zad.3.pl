6563ac21c8405278776b22bab03f120c9510c187
weronikatyminska <wera.tyminska@gmail.com>
2025-01-23 19:34:36 +0100

    Update prolog zad.3.pl

--- prolog zad.3.pl
+++ prolog zad.3.pl
@@ -9,7 +9,12 @@ dostepny_produkt(X) :- produkt(X,Y,_), Y > 0.
 % ?- dostepny_produkt(tv) wynik: true, ?- dostepny_produkt(stolik) wynik: false
 
 przenies_produkt(X,Z) :- produkt(X,Y,_), retract(produkt(X,Y,_)), assertz(produkt(X,Y,Z)), write(produkt(X,Y,Z)).
-% ?- przenies_produkt(pralka, sektor_a) wynik: produkt(pralka,5,sektor_a),true
+% ?- przenies_produkt(komoda, sektor_a). wynik: produkt(komoda,8,sektor_a)true. , ?- produkt(komoda, X,Y). X = 8, Y = sektor_a.
 
-uzupelnij_stan(X, N) :- produkt(X,Y,Z), A is Y + N, write(produkt(X,A,Z)). 
-% ?- uzupelnij_stoli(stolik,10) wynik: produkt(stolik,10,sektor_d), true                                                          
+% ta wersja nie do końca działa
+% uzupelnij_stan(X, N) :- produkt(X,Y,Z), A is Y + N, write(produkt(X,A,Z)). 
+% ?- uzupelnij_stan(stolik,10) wynik: produkt(stolik,10,sektor_d), true 
+
+% ulepszona wersja do SWI Prolog:
+uzupelnij_stan(X, N) :- produkt(X, Z, Y), A is Z + N, retract(produkt(X, Z, Y)), assertz(produkt(X, A, Y)), write(produkt(X, A, Y)). 
+% ?- uzupelnij_stan(stolik,10). wynik: produkt(stolik,10,sektor_d)true. ?- produkt(stolik,X,Y). X = 10, Y = sektor_d.
